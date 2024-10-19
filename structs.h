68858fb88bbc494ca930c5b67052bde160a7ad75
Manan Garg <m_garg@cs.iitr.ac.in>
2024-10-19 17:03:15 +0530

    file-parsing

---
+++ structs.h
@@ -0,0 +1,15 @@
+#include <fstream>
+
+#pragma once
+
+struct event
+{
+    bool is_send_event;
+    int pid;
+    int p_eid;
+    std::string s_pid;
+    std::string msg_id;
+    int sender_pid;
+};
+
+std::ostream &operator<<(std::ostream &s, event &e);
---
+++ utils.h
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <vector>
+#include "structs.h"
+#pragma once
+
+const std::string begin_process_prefix = "begin process p";
+const int begin_p_no_offset = begin_process_prefix.size();
+
+const std::string end_process_prefix = "end process p";
+const int end_p_no_offset = end_process_prefix.size();
+
+const std::string send_msg_prefix = "send m";
+const int msg_no_offset = send_msg_prefix.size();
+
+const std::string recv_b_prefix = "recv_B";
+
+std::vector<std::vector<event>> parse_commands(std::ifstream &s);
+
+std::vector<std::string> split_by_space(std::string str);
